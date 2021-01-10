59dc52a17655bcf9a0d55953218a9fdb10e4651f
Ian Bruns <ian@rbruns.com>
2021-01-10 14:30:51 -0700

    added tests and functionality for GET requests

---
+++ test/articles-endpoints.spec.js
@@ -0,0 +1,48 @@
+const { expect } = require('chai');
+const knex = require('knex');
+const supertest = require('supertest');
+const app = require('../src/app');
+const { makeArticlesArray } = require('./articles.fixtures');
+
+describe.only('Articles Endpoints', () => {
+    let db;
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        });
+        app.set('db', db);
+    });
+
+    after('disconnect from db', () => db.destroy());
+
+    before('clean the table', () => db('blogful_articles').truncate());
+
+    afterEach('cleanup', () => db('blogful_articles').truncate());
+
+    context('Given there are articles in the database', () => {
+        const testArticles = makeArticlesArray();
+
+        beforeEach('insert articles', () => {
+            return db
+                .into('blogful_articles')
+                .insert(testArticles);
+        });
+
+        it(`Get ./articles respons with a 200 and all of the articles`, () => {
+            return supertest(app)
+                .get('/articles')
+                .expect(200, testArticles);
+        });
+
+        it(`GET /articles/:article_id responds with a 200 and the specified article`, () => {
+            const articleId = 2;
+            const expectedArticle = testArticles[articleId - 1];
+
+            return supertest(app)
+                .get(`/articles/${articleId}`)
+                .expect(200, expectedArticle);
+        });
+    });
+});
